d0dabcffd5d43f4d25a34753458de329dc0ae0ba
Lucas Rocha <lucasrocha234@hotmail.com>
2025-05-15 21:27:40 -0300

    mat->python|Gerar populção incial - incompleto

--- main.py
+++ main.py
@@ -4,27 +4,27 @@ import numpy as np
 from scipy import sparse
 
 # Parametros ajustaveis manualmente
-nfp=5;             #Funcoes de pertinencia
-alfa=0.01;         #Alfa - parametro de atualizacao
-nepoca=5;        #Numero de epocas de treinamento dos pesos para cada geração da PG
+nfp=5             #Funcoes de pertinencia
+alfa=0.01         #Alfa - parametro de atualizacao
+nepoca=5       #Numero de epocas de treinamento dos pesos para cada geração da PG
 
 #Parametros Programacao Genetica
-tamPop=50;
-numGeracoes=100;
-taxaCruza=0.7;
-taxaMuta=0.08;
+tamPop=3
+numGeracoes=100
+taxaCruza=0.7
+taxaMuta=0.08
 nfpMax=7; # maximo de funções de pertinência geradas em cada indivíduo
 
-_xt_temp = np.loadtxt('d:\\ScientificIni\\Scientific-Initiation\\xt.csv', delimiter=',', skiprows=1)
+_xt_temp = np.loadtxt('xt.csv', delimiter=',', skiprows=1)
 xt = _xt_temp[:, 1:] 
 
-_yt_temp = np.loadtxt('d:\\ScientificIni\\Scientific-Initiation\\yt.csv', delimiter=',', skiprows=1)
+_yt_temp = np.loadtxt('yt.csv', delimiter=',', skiprows=1)
 yt = _yt_temp[:, 1:]
 
 npt, nin = (xt).shape
 
-nptTr = round(npt*0.6);
-nptVal = round(npt*0.2);
+nptTr = round(npt*0.6)
+nptVal = round(npt*0.2)
 
 xv = xt[nptTr-1:]
 xt = xt[:nptTr]
@@ -42,9 +42,27 @@ aux = sparse.csr_matrix(xt)
 #Calculo dos parametros
 p = np.empty((nin, nfp))
 q = np.empty(nfp)
-for i in range(0,nfp):
-    for j in range(0,nin):
+for i in range(nfp):
+    for j in range(nin):
         p[j, i] = np.random.rand()
 
     q[i] = np.random.rand()
+    
+#Gerar população inicial
+pop = []
+for z in range(tamPop):
+    nfpSort = nfp
+    cs = np.empty((nin, nfpSort))
+    ss = np.empty((nin, nfpSort))
+    for i in range(nfpSort):
+        for j in range(nin):
+            cs[j, i] = xmin[j] + np.random.rand()*(xmax[j]-xmin[j])
+            ss[j, i] = np.random.rand()*(xmax[j]-xmin[j])
 
+    individuo = {
+        'nfps': nfpSort,
+        'cs': cs,
+        'ss': ss
+    }
+    
+    individuo['fitness'] = (0.5 * sum((pop[0] ** 2)))
