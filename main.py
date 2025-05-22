b9895d155159421cec334842a8ed31164d494803
Fabricio de Assis Vaz <fabricio.vaz@gmail.com>
2025-05-22 19:58:24 -0300

    mat->python|Finalizou geração da pop inicial

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import numpy as np
 import saida
 import skfuzzy as fuzz
 from scipy import sparse
+import time
+start_time = time.time() 
 
 # Parametros ajustaveis manualmente
 nfp=5             #Funcoes de pertinencia
@@ -11,7 +13,7 @@ alfa=0.01         #Alfa - parametro de atualizacao
 nepoca=5       #Numero de epocas de treinamento dos pesos para cada geração da PG
 
 #Parametros Programacao Genetica
-tamPop=3
+tamPop=50
 numGeracoes=100
 taxaCruza=0.7
 taxaMuta=0.08
@@ -19,7 +21,6 @@ nfpMax=7; # maximo de funções de pertinência geradas em cada indivíduo
 
 _xt_temp = np.loadtxt('xt.csv', delimiter=',', skiprows=1)
 xt = _xt_temp[:, 1:] 
-
 _yt_temp = np.loadtxt('yt.csv', delimiter=',', skiprows=1)
 yt = _yt_temp[:, 1:]
 
@@ -50,9 +51,7 @@ for i in range(nfp):
 
     q[i] = np.random.rand()
     
-#Gerar população inicial
-saida.saida()
-
+# #Gerar população inicial
 pop = []
 
 for z in range(tamPop):
@@ -69,5 +68,27 @@ for z in range(tamPop):
         'cs': cs,
         'ss': ss
     }
+    individuo['saida'] = saida.saida(xt, individuo['cs'], individuo['ss'], p, q, individuo['nfps'])
+    individuo['fitness'] = (0.5 * np.sum((individuo['saida'] - ydt)**2)) / npt
+    pop.append(individuo)
+
+    if z == 0:
+        melhorindv = z
+    else:
+        if individuo['fitness'] < pop[melhorindv]['fitness']:
+            melhorindv = z
     
-    individuo['fitness'] = (0.5 * sum((pop[0] ** 2)))
+c = pop[melhorindv]['cs']
+s = pop[melhorindv]['ss']
+nfp = pop[melhorindv]['nfps']
+auxsize = tamPop
+novapop = pop
+
+
+
+
+
+
+end_time = time.time() 
+execution_time = end_time - start_time 
+print(f"The script took {execution_time:.4f} seconds to execute.")
--- saida.py
+++ saida.py
@@ -1,14 +1,27 @@
 import skfuzzy as fuzz
-
+import numpy
 #pop(z).saida=saida(xt,pop(z).cs,pop(z).ss,p,q,pop(z).nfps)
 def saida(x, c, s, p, q, m):
     np, n = (x).shape
+    ys = numpy.zeros(np)
     for i in range(np):
         a = 0
         b = 0
+        y = numpy.zeros(m)
+        w = numpy.ones(m)
         for j in range(m):
             y[j] = q[j]
             w[j] = 1
             for k in range(n):
                 w[j] *= fuzz.gaussmf(x[i,k], mean = c[k,j], sigma = s[k,j])
-                
+
+                y[j] += p[k,j] * x[i,k]
+
+            a += w[j]*y[j]
+            b += w[j]
+
+        if(b == 0):
+            b = 1
+        ys[i] = a/b
+
+        return ys
